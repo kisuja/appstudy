7b565ae780757f21c2587d8f6d3ad71a5466d62d
kisuja_ <tw060811@naver.com>
2024-01-19 18:53:32 +0900

    start app

---
+++ app/src/main/java/com/cookandroid/a4_1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cookandroid.a4_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("직접 풀어보기 4-2");
+    }
+}
